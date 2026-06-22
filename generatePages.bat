667aa972333acd90100972cb2a8e94d060d0c788
ImMalloy <lefehanims@gmail.com>
2026-06-22 13:27:25 -0600

    avoid haxe pagesGenerator.hxml

--- generatePages.bat
+++ generatePages.bat
@@ -1,3 +1,3 @@
 @echo off
 
-haxe pagesGenerator.hxml
+haxelib run dox -i ALE-Psych/docs.xml -o docs -theme themes/ale-psych --title "ALE Psych API" -D themeColor 0xB986FF -D textColor 0xF4F7FF -D website https://github.com/ALE-Psych-Crew/ALE-Psych -D description "API documentation for ALE Psych." -D source-path https://github.com/ALE-Psych-Crew/ALE-Psych/blob/main/source/
