08694cc481c8e1b591b4bfd57cfa7150795c3d80
ImMalloy <lefehanims@gmail.com>
2026-06-22 13:45:57 -0600

    wa

--- generatePages.bat
+++ generatePages.bat
@@ -1,3 +1,10 @@
 @echo off
 
-haxelib run dox -i ALE-Psych/docs.xml -o docs -theme themes/ale-psych --title "ALE Psych API" -D themeColor 0xB986FF -D textColor 0xF4F7FF -D website https://github.com/ALE-Psych-Crew/ALE-Psych -D description "API documentation for ALE Psych." -D source-path https://github.com/ALE-Psych-Crew/ALE-Psych/blob/main/source/
+if exist "ALE-Psych\docs.xml" (
+	set "ALE_PSYCH_XML=ALE-Psych\docs.xml"
+) else (
+	set "ALE_PSYCH_XML=..\ALE-Psych\docs.xml"
+)
+
+haxelib run dox -i "%ALE_PSYCH_XML%" -o docs -theme themes/ale-psych --title "ALE Psych API" -D themeColor 0xB986FF -D textColor 0xF4F7FF -D website https://github.com/ALE-Psych-Crew/ALE-Psych -D description "API documentation for ALE Psych." -D source-path https://github.com/ALE-Psych-Crew/ALE-Psych/blob/main/source/
+exit /b %ERRORLEVEL%
--- generateXML.bat
+++ generateXML.bat
@@ -1,3 +1,12 @@
-cd ALE-Psych
-haxe ../xmlGenerator.hxml
-cd ..
+@echo off
+
+set "ALE_PSYCH_PATH=%~1"
+if "%ALE_PSYCH_PATH%"=="" set "ALE_PSYCH_PATH=..\ALE-Psych"
+
+pushd "%ALE_PSYCH_PATH%"
+haxelib run lime display windows > docsGenerator.hxml
+echo -xml docs.xml>> docsGenerator.hxml
+haxe docsGenerator.hxml
+set "GEN_EXIT=%ERRORLEVEL%"
+popd
+exit /b %GEN_EXIT%
